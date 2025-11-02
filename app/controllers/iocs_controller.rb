d69c76848c2e0432246155b56bdfea853a81e2a1
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2025-11-02 08:21:01 +0000

    feat: implement CRUD operations for IOC model
    * feat: creates iocs routes
    
    * feat(iocs): add controller and views for listing, showing, and creating IOCs
    
    * feat(iocs): adds methods and views for edit and update, implements form partial
    
    * feat(iocs): adds destroy to controller and updates show view

---
+++ app/controllers/iocs_controller.rb
@@ -0,0 +1,47 @@
+class IocsController < ApplicationController
+  before_action :set_ioc, only: %i[ show edit update destroy ]
+  def index
+    @iocs = Ioc.all
+  end
+
+  def show
+  end
+
+  def new
+    @ioc = Ioc.new
+  end
+
+  def create
+    @ioc = Ioc.new(ioc_params.merge(source: "public"))
+    if @ioc.save
+      redirect_to @ioc
+    else render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ioc.update(ioc_params)
+      redirect_to @ioc
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @ioc.destroy
+    redirects_to iocs_path
+  end
+
+  private
+  def set_ioc
+    @ioc = Ioc.find(params[:id])
+  end
+
+  private
+  def ioc_params
+    params.expect(ioc: [ :ioc_type, :value, :first_seen ])
+  end
+end
